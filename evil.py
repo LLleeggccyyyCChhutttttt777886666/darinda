efec64709f4a76d15065429990aed6e0407e9b15
Legacychutt <yoginum2@outlook.com>
2024-12-27 17:03:23 +0530

    Update evil.py

--- evil.py
+++ evil.py
@@ -2,7 +2,7 @@ import asyncio
 from telegram import Update
 from telegram.ext import Application, CommandHandler, CallbackContext
 
-TELEGRAM_BOT_TOKEN = '7300928635:AAG-pNqG_2Mplw91A6xDfg_enMYRU6_jmTY'
+TELEGRAM_BOT_TOKEN = '7836401331:AAFZ-VJ_P4j8Yi6WCg9l5AbRh4bCR6Cvui4'
 ADMIN_USER_ID = 945650098
 USERS_FILE = 'users.txt'
 attack_in_progress = False
